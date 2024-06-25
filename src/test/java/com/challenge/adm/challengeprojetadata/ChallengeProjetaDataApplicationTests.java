1ff9f186cbc8be9d09b6e19979adc4884af75dc4
renarasecchim <renarasecchim@gmail.com>
2024-06-25 15:50:27 -0300

    feat: init project

---
+++ src/test/java/com/challenge/adm/challengeprojetadata/ChallengeProjetaDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.adm.challengeprojetadata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeProjetaDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
