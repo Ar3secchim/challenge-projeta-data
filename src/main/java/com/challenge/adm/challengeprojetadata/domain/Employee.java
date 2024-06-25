6c11a306e4eb56dfc6d54cb6969705514a21bc3b
renarasecchim <renarasecchim@gmail.com>
2024-06-25 15:51:04 -0300

    feat: create entity

---
+++ src/main/java/com/challenge/adm/challengeprojetadata/domain/Employee.java
@@ -0,0 +1,52 @@
+package com.challenge.adm.challengeprojetadata.domain;
+
+import com.challenge.adm.challengeprojetadata.utils.FormatterBigDecimal;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.text.NumberFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+
+@Entity
+@Table(name = "employee")
+@Getter
+@Setter
+public class Employee extends Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private BigDecimal salary;
+    private String position;
+
+    protected Employee() {
+    }
+
+    public Employee(String name, LocalDate birthDate, BigDecimal salary, String position) {
+        super(name, birthDate);
+        this.salary = salary;
+        this.position = position;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Nome: " + getName() +
+                ", Data de Nascimento: " + formatterDateTime(getBirthDate()) +
+                ", Salário: " + FormatterBigDecimal.forFormatBrazil(getSalary()) +
+                ", Função: " + getPosition();
+    }
+
+    private String formatterDateTime(LocalDate date) {
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        return date.format(dateFormatter);
+    }
+
+
+}
---
+++ src/main/java/com/challenge/adm/challengeprojetadata/domain/Person.java
@@ -0,0 +1,22 @@
+package com.challenge.adm.challengeprojetadata.domain;
+
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@MappedSuperclass
+public abstract class Person {
+    private String name;
+    private LocalDate birthDate;
+
+    protected Person() {
+    }
+
+    public Person(String name, LocalDate birthDate) {
+        this.name = name;
+        this.birthDate = birthDate;
+    }
+}
